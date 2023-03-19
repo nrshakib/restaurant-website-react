d458ce4e1ef54a8e42a4155bbd7b56e9faac2816
NRS\nrsha <nrshakib007@gmail.com>
2023-03-20 02:00:43 +0600

    home component

---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="pt-[8rem]  flex items-center xl:justify-center justify-start flex-wrap"
+            id="home">
+            <div className="flex flex-col items-start gap-10">
+                <div className="sm:text-[2.5rem] text-[1.8rem] font-bold">
+                    Delicious <br/> Food Is Waiting <br/> For You
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
